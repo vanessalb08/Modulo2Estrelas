80f7fd93bc5e8e9466b392ff2cafeb30ad749d42
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:27:59 -0300

    Adicionado exemplo 2

---
+++ br/com/zup/Aula1Exemplo2.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Aula1Exemplo2 {
+    public static void main(String[] args) {
+        //Armazenar valores na memória
+        int numero1 = 20;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+
+        System.out.println(soma);
+    }
+}
