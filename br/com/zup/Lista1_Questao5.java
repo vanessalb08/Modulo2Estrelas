e3e58bed63818726902768cf384f3f89ec910944
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 15:48:21 -0300

    Lista 1 questão 5

---
+++ br/com/zup/Lista1_Questao5.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class Lista1_Questao5 {
+    public static void main(String[] args) {
+        float fahrenheit = 73.4F;
+        float celsius = (fahrenheit - 32) / 1.8F;
+
+        System.out.println(fahrenheit + "°F equivalem a " + celsius + "°C");
+    }
+}
