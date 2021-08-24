9b514d986ab55da72145f4ed870b8eefa41d365c
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:18:43 -0300

    Concatenação

--- br/com/zup/Aula1_Exemplo1.java
+++ br/com/zup/Aula1_Exemplo1.java
@@ -4,9 +4,6 @@ public class Aula1_Exemplo1 {
     public static void main(String[] args) {
         int idadeDoUsuario = 18;
         String nomeDoUsuario = "Leticia";
-        System.out.print("Olá, meu nome é ");
-        System.out.print(nomeDoUsuario);
-        System.out.print(" e minha idade é ");
-        System.out.print(idadeDoUsuario);
-        System.out.print(" anos");
+        System.out.println("Meu nome é " + nomeDoUsuario + " e tenho " + idadeDoUsuario + " anos");
+    }
 }
