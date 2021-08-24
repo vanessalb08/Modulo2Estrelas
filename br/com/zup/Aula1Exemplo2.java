5e37943bb27aeea1617bbbf3390d4693bfbc9a3c
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:45:49 -0300

    Criando operação de subtração

--- br/com/zup/Aula1Exemplo2.java
+++ br/com/zup/Aula1Exemplo2.java
@@ -9,7 +9,13 @@ public class Aula1Exemplo2 {
         //Criando a operação de soma
         int soma = numero1 + numero2;
 
+        //Criando a operação subtração
+        int resultadoSubtracao = numero1 - numero2;
+
         //Exibindo a operação para o usuário
         System.out.println(soma);
+
+        //Exibindo o resultado da subtração
+        System.out.println("O resultado da soma é " + soma + " e o da subtração é " + resultadoSubtracao);
     }
 }
