67bf401890970d9495152ba88a024431b675223a
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 15:37:33 -0300

    Lista 1 questão 3

---
+++ br/com/zup/Lista1_Questao3.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class Lista1_Questao3 {
+    public static void main(String[] args) {
+        float metros = 3.5F;
+        float centimetros = metros * 100;
+
+        System.out.println(metros + " equivale a " + centimetros + " centímetros");
+    }
+}
