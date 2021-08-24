a0afb2449ac9d88bd9710077958a1eead051ddc2
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:49:39 -0300

    Adicionando multiplicação e divisão

--- br/com/zup/Aula1Exemplo2.java
+++ br/com/zup/Aula1Exemplo2.java
@@ -12,10 +12,15 @@ public class Aula1Exemplo2 {
         //Criando a operação subtração
         int resultadoSubtracao = numero1 - numero2;
 
+        //Criando multiplicação e divisão
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+
         //Exibindo a operação para o usuário
         System.out.println(soma);
 
         //Exibindo o resultado da subtração
         System.out.println("O resultado da soma é " + soma + " e o da subtração é " + resultadoSubtracao);
+        System.out.println("O resultado da multiplicação é " + multiplicacao + " e o da divisão é " + divisao);
     }
 }
