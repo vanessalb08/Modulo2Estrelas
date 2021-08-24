855a3ea01049c0d36d4ae534fd1010ea6c863d20
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:32:52 -0300

    Criando operaçãod e soma

--- br/com/zup/Aula1Exemplo2.java
+++ br/com/zup/Aula1Exemplo2.java
@@ -5,8 +5,11 @@ public class Aula1Exemplo2 {
         //Armazenar valores na memória
         int numero1 = 20;
         int numero2 = 20;
+
+        //Criando a operação de soma
         int soma = numero1 + numero2;
 
+        //Exibindo a operação para o usuário
         System.out.println(soma);
     }
 }
