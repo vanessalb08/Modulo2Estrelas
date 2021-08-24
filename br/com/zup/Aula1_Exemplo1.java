65a6b14430e193c99061bad3e40a8c91e8548640
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 14:16:18 -0300

    Primeiro commit do dia 24/08

---
+++ br/com/zup/Aula1_Exemplo1.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Aula1_Exemplo1 {
+    public static void main(String[] args) {
+        int idadeDoUsuario = 18;
+        String nomeDoUsuario = "Leticia";
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print(" e minha idade é ");
+        System.out.print(idadeDoUsuario);
+        System.out.print(" anos");
+}
