378ed70b9ef546d62632f3192376c45367ba23ac
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-24 15:35:12 -0300

    Lista 1 Questão 2

---
+++ br/com/zup/Lista1_Questao2.java
@@ -0,0 +1,16 @@
+package br.com.zup;
+
+public class Lista1_Questao2 {
+    public static void main(String[] args) {
+        float nota1 = 8;
+        float nota2 = 9.3F;
+        float nota3 = 4.6F;
+        float nota4 = 8.2F;
+
+        float soma = nota1 + nota2 + nota3 + nota4;
+        float media = soma / 4;
+
+        System.out.println("A média artmética é " + media);
+
+    }
+}
