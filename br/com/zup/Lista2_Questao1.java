26847e76fe9478f9a0f31ffb91b8340a420795b7
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-25 12:12:23 -0300

    Lista 2 Questão 1, início do desafio e introdução ao Scanner

--- br/com/zup/Lista2_Questao1.java
+++ br/com/zup/Lista2_Questao1.java
@@ -1,8 +1,12 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Lista2_Questao1 {
     public static void main(String[] args) {
-        float peso = 51.3F;
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual o peso dos peixes?");
+        float peso = leitor.nextFloat();
         float excesso = peso - 50;
         float multa = excesso * 4;
 
