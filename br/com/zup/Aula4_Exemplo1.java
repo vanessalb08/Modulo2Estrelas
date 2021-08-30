fc8e885140c07cd5462739bab232c7a357553f1d
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-08-30 13:38:39 -0300

    Resolução da lista de exercício 3

---
+++ br/com/zup/Aula4_Exemplo1.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Aula4_Exemplo1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double valorTotal = 0;
+
+    }
+}
