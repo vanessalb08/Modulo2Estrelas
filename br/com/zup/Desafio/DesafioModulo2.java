485823ceca0d589ea0d79a5a036ddf195a862eb1
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-03 14:57:11 -0300

    Adicionada opção de excluir funcionário

--- br/com/zup/Desafio/DesafioModulo2.java
+++ br/com/zup/Desafio/DesafioModulo2.java
@@ -14,6 +14,7 @@ public class DesafioModulo2 {
         String email;
         String telefone;
         String cpf;
+        String excluir;
         int opcao;
         boolean executarPrograma = true;
 
@@ -25,7 +26,8 @@ public class DesafioModulo2 {
             System.out.println("Digite a opção desejada:");
             System.out.println("\t1 - Adicionar funcionários");
             System.out.println("\t2 - Exibir lista de funcionários");
-            System.out.println("\t3 - Encerrar programa");
+            System.out.println("\t3 - Excluir funcionário");
+            System.out.println("\t4 - Encerrar programa");
 
             //Ler a opção do usuário
             opcao = leitor.nextInt();
@@ -48,13 +50,37 @@ public class DesafioModulo2 {
                 System.out.println("Funcionário cadastrado com sucesso!");
                 System.out.println("____________________________________\n");
             }
+            //Mostrar lista de funcionários
             else if (opcao == 2){
-                System.out.println("--------Funcionários cadastrados:--------");
-                System.out.println(funcionarios);
+                //Mostrar mensageme caso a lista esteja vazia
+                if (funcionarios.size() == 0){
+                    System.out.println("Ainda não há funcionários cadastrados, por favor cadastre.");
+                }
+                else{
+                    System.out.println("--------Funcionários cadastrados:--------");
+                    System.out.println(funcionarios);
+                }
             }
+            //Excluir funcionário
             else if (opcao == 3){
-                executarPrograma = false;
+                //Verificar se a lista está vazia
+                if (funcionarios.size() == 0){
+                    System.out.println("Não há funcionários cadastrados, por favor cadastre primeiro.");
+                }
+                //Caso não esteja, excluir
+                else{
+                    System.out.println("Digite o CPF do funcionario que deseja excluir:");
+                    excluir = leitor.nextLine();
+                    funcionarios.remove(excluir);
+                    System.out.println("Funcionário com CPF " + excluir + " excluído com sucesso");
+                }
             }
+            //Sair do programa
+            else if (opcao == 4){
+                executarPrograma = false;
+                System.out.println("Até mais!");
+            }
+            //Caso o usuário digite uma opção que não está no menu
             else{
                 System.out.println("Por favor, digite uma opção válida");
             }
