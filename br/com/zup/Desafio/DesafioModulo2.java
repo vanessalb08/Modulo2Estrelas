6a875dbb443739d76a7e9cf5438640597e8b35bf
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-03 11:50:39 -0300

    Adicionado menu com opções de adicionar, exibir lista e encerrar programa

--- br/com/zup/Desafio/DesafioModulo2.java
+++ br/com/zup/Desafio/DesafioModulo2.java
@@ -14,22 +14,51 @@ public class DesafioModulo2 {
         String email;
         String telefone;
         String cpf;
+        int opcao;
+        boolean executarPrograma = true;
 
-        //Solicitando dados ao usuário
         System.out.println("==========Seja bem vinde!==========");
-        System.out.println("Digite o nome completo do funcionário:");
-        nome = leitor.nextLine();
-        System.out.println("Digite o telefone do funcionário:");
-        telefone = leitor.nextLine();
-        System.out.println("Digite o e-mail do funcionário:");
-        email = leitor.nextLine();
-        System.out.println("Digite o CPF do funcionário:");
-        cpf = leitor.nextLine();
-        System.out.println("Funcionário cadastrado com sucesso!");
 
-        //Adicionando os dados a hashMap
-        funcionarios.put(cpf, "Nome: " + nome + "/ Telefone: "+ telefone + "/ E-mail: "+ email);
+        while (executarPrograma == true){
 
-        System.out.println(funcionarios);
+            //Menu de opções para o usuário
+            System.out.println("Digite a opção desejada:");
+            System.out.println("\t1 - Adicionar funcionários");
+            System.out.println("\t2 - Exibir lista de funcionários");
+            System.out.println("\t3 - Encerrar programa");
+
+            //Ler a opção do usuário
+            opcao = leitor.nextInt();
+            leitor.nextLine();
+
+            //Adicionando funcionário
+            if (opcao == 1){
+                System.out.println("Digite o nome completo do funcionário:");
+                nome = leitor.nextLine();
+                System.out.println("Digite o telefone do funcionário:");
+                telefone = leitor.nextLine();
+                System.out.println("Digite o e-mail do funcionário:");
+                email = leitor.nextLine();
+                System.out.println("Digite o CPF do funcionário:");
+                cpf = leitor.nextLine();
+
+                //Adicionando os dados a hashMap
+                funcionarios.put(cpf,"Nome: " + nome + "/ Telefone: "+ telefone + "/ E-mail: "+ email +"\n");
+                System.out.println("\n____________________________________");
+                System.out.println("Funcionário cadastrado com sucesso!");
+                System.out.println("____________________________________\n");
+            }
+            else if (opcao == 2){
+                System.out.println("--------Funcionários cadastrados:--------");
+                System.out.println(funcionarios);
+            }
+            else if (opcao == 3){
+                executarPrograma = false;
+            }
+            else{
+                System.out.println("Por favor, digite uma opção válida");
+            }
+        }
+
     }
 }
