7473b345f47e12fac797c5572040499a77f9e955
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-03 10:44:46 -0300

    Primeiro commit - criando o arquivo

---
+++ br/com/zup/Desafio/DesafioModulo2.java
@@ -0,0 +1,12 @@
+package br.com.zup.Desafio;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DesafioModulo2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map<String,String> funcionarios = new HashMap<String,String>();
+    }
+}
