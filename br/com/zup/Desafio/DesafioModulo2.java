d972c111114fec36c4dfaa163dc523db6d96fe83
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-09-03 11:08:14 -0300

    Inserindo ao menos um funcionário e exibindo na tela

--- br/com/zup/Desafio/DesafioModulo2.java
+++ br/com/zup/Desafio/DesafioModulo2.java
@@ -8,5 +8,28 @@ public class DesafioModulo2 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         Map<String,String> funcionarios = new HashMap<String,String>();
+
+        //Criando as variáveis necessárias
+        String nome;
+        String email;
+        String telefone;
+        String cpf;
+
+        //Solicitando dados ao usuário
+        System.out.println("==========Seja bem vinde!==========");
+        System.out.println("Digite o nome completo do funcionário:");
+        nome = leitor.nextLine();
+        System.out.println("Digite o telefone do funcionário:");
+        telefone = leitor.nextLine();
+        System.out.println("Digite o e-mail do funcionário:");
+        email = leitor.nextLine();
+        System.out.println("Digite o CPF do funcionário:");
+        cpf = leitor.nextLine();
+        System.out.println("Funcionário cadastrado com sucesso!");
+
+        //Adicionando os dados a hashMap
+        funcionarios.put(cpf, "Nome: " + nome + "/ Telefone: "+ telefone + "/ E-mail: "+ email);
+
+        System.out.println(funcionarios);
     }
 }
